b47c0afbd1f7f73eb2c59ee2d913c80f51e80cfd
PRERNA SHARMA <prernasharma@PRERNAs-MacBook-Air.local>
2026-07-14 22:11:17 +0530

    Modified llm.py to use HuggingFace Client for Deployment

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Intelligent News Assistant
 
-## An AI-powered news analysis platform that leverages open-source Large Language Models (LLMs) to summarize news articles, analyze sentiment, extract key entities and keywords, and answer user questions through prompt engineering.
+### An AI-powered news analysis platform that leverages open-source Large Language Models (LLMs) to summarize news articles, analyze sentiment, extract key entities and keywords, and answer user questions through prompt engineering.
+
+### Install Requirements:
+#### pip install -r requirements.txt
+
+### Command to run Streamlit app
+#### python -m streamlit run app.py
--- services/llm.py
+++ services/llm.py
@@ -1,14 +1,33 @@
-from langchain_ollama import ChatOllama
-from config import OLLAMA_MODEL
+# from langchain_ollama import ChatOllama
+# from config import OLLAMA_MODEL
+import os
+from dotenv import load_dotenv
+from huggingface_hub import InferenceClient
 
+load_dotenv()
 
 class LLMService:
     def __init__(self):
-        self.llm = ChatOllama(
-            model=OLLAMA_MODEL,
-            temperature=0.2
+        self.client = InferenceClient(
+            api_key=os.getenv("HF_TOKEN")
         )
+        self.model = os.getenv("HF_MODEL")
 
     def generate(self, prompt: str):
-        response = self.llm.invoke(prompt)
-        return response.content
+        completion = self.client.chat.completions.create(
+
+            model=self.model,
+
+            messages=[
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+
+            max_tokens=600,
+
+            temperature=0.3,
+        )
+
+        return completion.choices[0].message.content
